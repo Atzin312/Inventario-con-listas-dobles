63e53e2ceee1c242c2d2731ad36f3716c0ede3c4
Atzin312 <amoreno9@ucol.mx>
2017-05-12 09:23:46 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Control_de_Inventario
+{
+    public partial class Form1 : Form
+    {
+        Inventario inventario = new Inventario();
+        Producto producto;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            int codigo = Convert.ToInt32(txtCodigo.Text);
+            if (inventario.buscarProducto(codigo) != null)
+            {
+                txtReporte.Text = inventario.buscarProducto(codigo).nombre;
+            }
+            else
+            {
+                txtReporte.Clear();
+            }
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReporte_Click(object sender, EventArgs e)
+        {
+            txtReporte.Text = inventario.ToString();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            inventario.eliminarProducto(Convert.ToInt32(txtCodigo.Text));
+        }
+
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            Producto producto = new Producto(Convert.ToInt32(txtCodigo.Text), txtNombre.Text, Convert.ToInt32(txtCantidad.Text), Convert.ToInt32(txtCosto.Text));
+            inventario.agregarProducto(producto);
+            txtCodigo.Focus();
+        }
+
+        private void btnFinal_Click(object sender, EventArgs e)
+        {
+            Producto producto = new Producto(Convert.ToInt32(txtCodigo.Text), txtNombre.Text, Convert.ToInt32(txtCantidad.Text), Convert.ToInt32(txtCosto.Text));
+            inventario.agregarInicio(producto);
+            txtCodigo.Focus();
+        }
+
+        private void btnInsertar_Click_1(object sender, EventArgs e)
+        {
+            Producto producto = new Producto(Convert.ToInt32(txtCodigo.Text), txtNombre.Text, Convert.ToInt32(txtCantidad.Text), Convert.ToInt32(txtCosto.Text));
+            inventario.insertarProducto(producto, Convert.ToInt32(txtPocision.Text));
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtReporte.Text = inventario.reporteInverso();
+        }
+
+        private void btnEliminarPrimerProducto_Click(object sender, EventArgs e)
+        {
+            inventario.eliminarPrimerProducto();
+        }
+
+        private void btnEliminarUltimoProducto_Click(object sender, EventArgs e)
+        {
+            inventario.eliminarUltimoProducto();
+        }
+    }
+}
---
+++ Inventario.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_de_Inventario
+{
+    class Inventario
+    {
+        Producto producto;
+        private Producto[] _Inventario;
+        private int _contador;
+        Producto auxiliar;
+        //int dato;
+        //Producto siguiente;
+        Producto inicio;
+        Producto final;
+
+        public Inventario()
+        {
+            inicio = null;
+            final = null;
+        }
+
+        public bool vacia()
+        {
+            if (inicio == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void agregarProducto(Producto producto)
+        {
+            Producto tmp = producto;
+
+            if (vacia())
+            {
+                inicio = tmp;
+                final = tmp;
+            }
+            else
+            {
+                final.siguiente = tmp;
+                tmp.anterior = final;
+                final = tmp;
+            }
+        }
+
+        public void agregarInicio(Producto producto)
+        {
+            Producto tmp = producto;
+
+            tmp.siguiente = inicio;
+           inicio.anterior = tmp;
+            inicio = tmp;
+        }
+
+        public void eliminarProducto(int posicion)
+        {
+            Producto anterior = inicio;
+            Producto actual = inicio;
+
+            int cont = 0;
+
+            if (posicion == 1)
+            {
+                inicio= inicio.siguiente;
+                inicio.anterior = null;
+            }
+            else if (posicion > cont)
+            {
+                while (posicion != cont && actual.siguiente != null)
+                {
+                    anterior = actual;
+                    actual = actual.siguiente;
+                    cont++;
+                }
+                anterior.siguiente = actual.siguiente;
+            }
+        }            
+
+        public void eliminarPrimerProducto()
+        {
+            inicio = inicio.siguiente;
+            inicio.anterior = null;
+        }
+
+        public void eliminarUltimoProducto()
+        {
+            final = final.anterior;
+            final.siguiente = null;
+        }
+
+        public Producto buscarProducto(int posicion)
+        {
+            Producto tmp = inicio;
+
+            while (tmp != null && tmp.codigo != posicion)
+            {
+                tmp = tmp.siguiente;
+            }
+
+            return tmp;
+        }
+
+        public void insertarProducto(Producto producto, int posicion)
+        {
+            int cont = 1;
+            Producto tmp = producto;
+
+            if (posicion == 1)
+            {
+                tmp.siguiente = inicio;
+               inicio.anterior = tmp;
+                inicio = tmp;
+            }
+            else
+            {
+                while (cont != posicion && inicio.siguiente != null)
+                {
+                    cont++;
+                    inicio = inicio.siguiente;
+                }
+                tmp.siguiente =inicio;
+                inicio.anterior = tmp;
+                tmp.anterior = inicio.anterior;
+                inicio = tmp;
+            }
+        }
+
+           
+        public string reporteInverso()
+        {
+            string mostrar = " ";
+            Producto tmp = final;
+
+            while (tmp != null)
+            {
+                mostrar += tmp.ToString();
+                tmp = tmp.anterior;
+            }
+            return mostrar;
+        }
+
+        public override string ToString()
+        {
+            string mostrar = "";
+            Producto tmp = inicio;
+
+            while (tmp != null)
+            {
+                mostrar += tmp.ToString();
+                tmp = tmp.siguiente;
+            }
+            return mostrar;
+        }
+    }
+}
+
+
